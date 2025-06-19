5cc2a91ed538c412d9cca63dbabbc36acb5ff501
fenek <sahasubbota@gmail.com>
2025-06-19 21:51:57 +0400

    add life span

--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
 from api.routes import main_router
 from config.settings import config
+from contextlib import asynccontextmanager
+from repository.database.base import BaseRepository
 import uvicorn
 
 app = FastAPI(
-    
+    lifespan=lifespan
 )
 app.include_router(main_router)
 
-
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        print("Data Table is Create")
+    yield 
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        print("Data Table is drop")
 
 if __name__=="__main__":
     uvicorn.run(
